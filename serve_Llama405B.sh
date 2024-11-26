f408f7aa591d87f0ae980420e25fbc09c58f20b6
sm1lla <foxsmilla@gmail.com>
2024-11-26 13:33:12 +0100

    RAG with PySpark API reference

--- serve_Llama405B.sh
+++ serve_Llama405B.sh
@@ -2,4 +2,4 @@
 
 # Run with "bash serve_Llama405B.sh"
 CUDA_VISIBLE_DEVICES=0,1,2,3;
-vllm serve neuralmagic/Meta-Llama-3.1-405B-Instruct-quantized.w4a16 --trust-remote-code --max-model-len 4096 --tensor-parallel-size 4 --api-key token-abc123
+vllm serve neuralmagic/Meta-Llama-3.1-405B-Instruct-quantized.w4a16 --trust-remote-code --max-model-len 8192 --tensor-parallel-size 4 --api-key token-abc123
